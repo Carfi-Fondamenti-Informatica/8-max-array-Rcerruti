ca472dc3da16f62c333c5808021298268bf816b8
Rcerruti <91542393+Rcerruti@users.noreply.github.com>
2021-12-11 20:39:15 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,6 +1,6 @@
 #ifndef ESERCITAZIONE_MAX_ARRAY_LIB_H
 #define ESERCITAZIONE_MAX_ARRAY_LIB_H
 
-float max_array(float , int);
+float max_array(float [] , int);
 
 #endif //ESERCITAZIONE_MAX_ARRAY_LIB_H
