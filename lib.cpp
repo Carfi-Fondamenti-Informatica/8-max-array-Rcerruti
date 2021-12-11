7ac4367e2dcaa951187b8f4e0616f9fd63220aa1
Rcerruti <91542393+Rcerruti@users.noreply.github.com>
2021-12-11 20:20:24 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,11 @@
-
+#include <iostream>
+#include lib.h'
+float max_array(float a[], int dim ) {
+    float max=a[0];
+    for (int i=0; i< dim; i++) {
+        if (a[i]>max) {
+            max=a[i];
+        }
+    }
+    return max;
+}
