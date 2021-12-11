e72375b8c089bc03ed6f96e7a730f8c6a4118c14
Rcerruti <91542393+Rcerruti@users.noreply.github.com>
2021-12-11 20:22:08 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
+using namespace std;
+#include "lib.h"
 
-int main(){
-  return 0;
-}
+
+
+    int main() {
+        int dimensione=0;
+        cin>>dimensione;
+        float array[dimensione];
+        for( int i=0; i<dimensione; i++){
+            cin>>array[i];
+        }
+        cout<< max_array(array, dimensione);
+        return 0;
+    }
